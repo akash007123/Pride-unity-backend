e4706fa839bb1d08298aa8e0b3f1d1bae18e3e73
Akash Raikwar <akashraikwar.mongoosetech@gmail.com>
2026-02-16 12:21:25 +0530

    vgvhhj

--- src/server.js
+++ src/server.js
@@ -16,7 +16,7 @@ connectDB();
 
 // Middleware
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+  origin: true,
   credentials: true
 }));
 app.use(express.json());
@@ -57,7 +57,7 @@ app.use((err, req, res, next) => {
 });
 
 // Start server
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running in ${process.env.NODE_ENV || 'development'} mode on port ${PORT}`);
